eee25288ec41c98124776296c82732e35e46d1cf
Moon Yewan <mool2480@gmail.com>
2021-08-11 17:06:13 +0900

    Entity-warn

--- Jpa_entity/src/main/java/jpabook/jpashop/Order.java
+++ Jpa_entity/src/main/java/jpabook/jpashop/Order.java
@@ -9,6 +9,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Table(name= "orders")
 @Getter @Setter
@@ -17,19 +19,36 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name="member_id")
     private Member member;
     //하나의 회원이 여러개를 주문하기떄문에 매니투원
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
+
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문 상태 [ORDER, CANCEL]
+
+    //연관관계 편의 메소드//
+    // 양방향일때//
+    public void  setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    //이 메소드 위치는 핵심적으로 컨트롤쪽에
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
